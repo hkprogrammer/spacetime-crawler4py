4233a07e1686daa5ef9909639f5a8483e3d06389
hitoki <hkidahas@uci.edu>
2023-05-05 15:44:00 -0700

    finalized along with report.pdf

--- scraper.py
+++ scraper.py
@@ -214,7 +214,7 @@ def is_valid(url):
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv|ppsx"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz|bib|pptx|ppsx|ppt|odc|txt)$", parsed.path.lower())
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz|bib|pptx|ppsx|ppt|odc|txt|apk)$", parsed.path.lower())
 
     except TypeError:
         print ("TypeError for ", parsed)
